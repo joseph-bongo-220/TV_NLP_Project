b317c164ed75a7d78b64726648e25b00c92a5ab6
Joseph-Bongo <joebongo220@gmail.com>
2019-04-28 19:19:33 -0400

    initial commit

---
+++ NLP.py
@@ -0,0 +1,421 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 11 01:29:33 2018
+
+@author: Joe
+"""
+
+"""Use My Own Kick Ass Algorithm to Extract Keyphrases"""
+from nltk.tokenize import word_tokenize
+import nltk
+nltk.download('stopwords')
+
+import pickle
+import pandas as pd 
+
+import inflect
+
+from collections import Counter
+import re
+import numpy as np
+from Scraper import Genius_TV_Scraper
+
+import networkx as nx
+
+# return second element of tuple within a list
+def second(elem):
+    """takes second item of list"""
+    return elem[1]
+
+# flatten a list of lists
+def flatten(x):
+    """Flattens a list of lists into a single list"""
+    flattened = [val for sublist in x for val in sublist]
+    return flattened
+
+def sort_dict(dict_unsorted):
+    dict_sorted = sorted(dict_unsorted.items(), key=lambda x: x[1], reverse=True)
+    return dict_sorted
+    
+def pickle_data_frames():
+    """Scrapes Genius scripts and saves them as Pickle files to be accessed later and to be easily called by API."""
+    GOT_Scraper = Genius_TV_Scraper(show='Game of thrones')
+    GOT_data = GOT_Scraper.get_scripts()
+    GOT_data.to_pickle('GOT_Pickle.pkl')
+    Office_Scraper = Genius_TV_Scraper(show='The office us')
+    Office_data = Office_Scraper.get_scripts()
+    Office_data.to_pickle('Office_Pickle.pkl')
+
+def process_episodes(show, seasons=None):
+
+    # initialize scraper for desire show and seasons (default for seasons is all available on Genius)
+    scraper = Genius_TV_Scraper(show=show, seasons=seasons)
+
+    #scrape previously specified show and seasons
+    data = scraper.get_scripts()
+
+    #get episodes from data
+    z = [sorted(data["Episode"], key=data["Episode"].count, reverse=True)[0]]
+    for i in sorted(data["Episode"], key=data["Episode"].count, reverse=True):
+        if i not in z:
+            z.append(i)
+    
+    docs=[]
+    for episode in z:
+        df = data[data.Episode == episode]
+        doc = " ".join(list(df["Line"]))
+        docs = docs.append(doc)
+
+    return docs
+
+def process_characters(show, seasons=None, num_char=10):
+
+    # initialize scraper for desire show and seasons (default for seasons is all available on Genius)
+    scraper = Genius_TV_Scraper(show=show, seasons=seasons)
+
+    # scrape previously specified show and seasons
+    data = scraper.get_scripts()
+
+    z = [sorted(data["Character_Name"], key=data["Character_Name"].count, reverse=True)[0]]
+    for i in sorted(data["Character_Name"], key=data["Character_Name"].count, reverse=True):
+        if i not in z:
+            z.append(i)
+
+    for char in z[0:num_char]:
+        df = data[data.Character_Name == char]
+        char_text = " ".join(list(df["Line"]))
+        doc = textacy.Doc(char_text, lang=u'en')
+        docs.append(doc)
+    corpus = textacy.Corpus(lang=u'en', docs=docs)
+    idf_dict = corpus.word_doc_freqs(normalize=None, weighting="idf", as_strings=True)
+
+    return docs, idf_dict
+
+def clean_text(words):
+    """Remove non-ASCII characters from list of tokenized words"""
+    new_words = []
+    for word in words:
+        new_word = re.sub(r'[^\w\s]', '', word)
+        if new_word != '':
+            new_words.append(new_word)
+    return new_words
+
+def remove_stopwords(words):
+    """Removes English Stopwords"""
+    new_words = []
+    for word in words:
+        if word not in stopwords.words('english'):
+            new_words.append(word)
+    return new_words
+
+def replace_numbers(words):
+    """Replace all interger occurrences in list of tokenized words with textual representation"""
+    rep_num = inflect.engine()
+    new_words = []
+    for word in words:
+        if word.isdigit():
+            new_word = rep_num.number_to_words(word)
+            new_words.append(new_word)
+        else:
+            new_words.append(word)
+    return new_words
+
+def get_BOW_TF(words):
+    """returns a dictionary with the term frequency of all terms in a given document"""
+    bagofwords = dict(Counter(words))
+    #Sum = sum(bagofwords.values())
+    
+#    for key, val in bagofwords.items():
+#        bagofwords[key] = val
+    return bagofwords
+
+def get_BOW_IDF(list_of_bows):
+    """returns a dictionary with the inverse document frequency of all terms in a given corpus"""
+    temp = []
+    for i in list_of_bows:
+        temp.append(i.keys())
+    
+    term_list = flatten(temp)
+    
+    idf_list=dict(Counter(term_list))
+    
+    for key, val in idf_list.items():
+        if len(re.findall("_", key))==0:
+            idf_list[key] = np.log(len(list_of_bows)/val)
+            
+        else:
+            idf_list[key] = np.log(len(list_of_bows))
+    
+    return idf_list
+
+def get_TFIDF_info(tf_dicts, idf_dict):
+    """takes in the term frequency dictionaries and IDF dictionary anc calculates TFIDF for every term in the corpus"""
+    tf_idf_dict = tf_dicts.copy()
+    for i in range(0,len(tf_idf_dict)):
+        for key, val in tf_idf_dict[i].items():
+            tf_idf_dict[i][key] = tf_idf_dict[i][key]*idf_dict[key]
+#    list1 = [x.items() for x in tf_idf_dict]
+#    foo = [list(x) for x in list1]
+#    for i in range(0,len(foo)):
+#        foo[i].sort(key=second, reverse=True)
+#    return foo
+    return tf_idf_dict
+
+def get_PFO(tokenized_doc, cutoff_position):
+    """Get the position of first occurance for a given word in a document"""
+    PFO_dict={}
+    for i in range(1,len(tokenized_doc)+1):
+        nums = [i for i in range(1,len(tokenized_doc)+1)]
+        word = tokenized_doc[-i]
+        if word in PFO_dict==True:
+            PFO_dict[word]=nums[-i]
+        else:
+            PFO_dict.update({word:nums[-i]})
+    
+    for key, val in PFO_dict.items():
+        PFO_dict[key] = np.log(cutoff_position/val)  
+            
+    return PFO_dict
+
+def get_TL(tokenized_doc):
+    tl_dict={}
+    for word in tokenized_doc:
+        tl_dict.update({word:np.log(len(re.findall("_", word))+2)})
+        
+    return tl_dict
+
+def subsum_correction(tf_dict_list):
+    for tf_dict in tf_dict_list:
+        for key, val in tf_dict.items():
+            if len(key.split(sep="_")) >= 2:
+                for word in key.split(sep="_"):
+                    tf_dict[word]=tf_dict[word]-tf_dict[key]
+                
+    return tf_dict_list
+    
+        
+class MyAlgo(object):
+    def __init__(self, docs, ngrams=[1,2,3,4,5,6], cutoff=1000, take_top=50):
+        cList = {
+                "ain't": "am not",
+                "aren't": "are not",
+                "can't": "cannot",
+                "can't've": "cannot have",
+                "'cause": "because",
+                "could've": "could have",
+                "couldn't": "could not",
+                "couldn't've": "could not have",
+                "didn't": "did not",
+                "doesn't": "does not",
+                "don't": "do not",
+                "em'": "them",
+                "'em": "them",
+                "hadn't": "had not",
+                "hadn't've": "had not have",
+                "hasn't": "has not",
+                "haven't": "have not",
+                "he'd": "he would",
+                "he'd've": "he would have",
+                "he'll": "he will",
+                "he'll've": "he will have",
+                "he's": "he is",
+                "how'd": "how did",
+                "how'd'y": "how do you",
+                "how'll": "how will",
+                "how's": "how is",
+                "i'd": "i would",
+                "i'd've": "i would have",
+                "i'll": "i will",
+                "i'll've": "i will have",
+                "i'm": "i am",
+                "i've": "i have",
+                "isn't": "is not",
+                "it'd": "it had",
+                "it'd've": "it would have",
+                "it'll": "it will",
+                "it'll've": "it will have",
+                "it's": "it is",
+                "let's": "let us",
+                "ma'am": "madam",
+                "mayn't": "may not",
+                "might've": "might have",
+                "mightn't": "might not",
+                "mightn't've": "might not have",
+                "must've": "must have",
+                "mustn't": "must not",
+                "mustn't've": "must not have",
+                "needn't": "need not",
+                "needn't've": "need not have",
+                "o'clock": "of the clock",
+                "oughtn't": "ought not",
+                "oughtn't've": "ought not have",
+                "shan't": "shall not",
+                "sha'n't": "shall not",
+                "shan't've": "shall not have",
+                "she'd": "she would",
+                "she'd've": "she would have",
+                "she'll": "she will",
+                "she'll've": "she will have",
+                "she's": "she is",
+                "should've": "should have",
+                "shouldn't": "should not",
+                "shouldn't've": "should not have",
+                "so've": "so have",
+                "so's": "so is",
+                "that'd": "that would",
+                "that'd've": "that would have",
+                "that's": "that is",
+                "there'd": "there had",
+                "there'd've": "there would have",
+                "there's": "there is",
+                "they'd": "they would",
+                "they'd've": "they would have",
+                "they'll": "they will",
+                "they'll've": "they will have",
+                "they're": "they are",
+                "they've": "they have",
+                "to've": "to have",
+                "wasn't": "was not",
+                "we'd": "we had",
+                "we'd've": "we would have",
+                "we'll": "we will",
+                "we'll've": "we will have",
+                "we're": "we are",
+                "we've": "we have",
+                "weren't": "were not",
+                "what'll": "what will",
+                "what'll've": "what will have",
+                "what're": "what are",
+                "what's": "what is",
+                "what've": "what have",
+                "when's": "when is",
+                "when've": "when have",
+                "where'd": "where did",
+                "where's": "where is",
+                "where've": "where have",
+                "who'll": "who will",
+                "who'll've": "who will have",
+                "who's": "who is",
+                "who've": "who have",
+                "why's": "why is",
+                "why've": "why have",
+                "will've": "will have",
+                "won't": "will not",
+                "won't've": "will not have",
+                "would've": "would have",
+                "wouldn't": "would not",
+                "wouldn't've": "would not have",
+                "y'all": "you all",
+                "y'alls": "you alls",
+                "y'all'd": "you all would",
+                "y'all'd've": "you all would have",
+                "y'all're": "you all are",
+                "y'all've": "you all have",
+                "you'd": "you had",
+                "you'd've": "you would have",
+                "you'll": "you wil",
+                "you'll've": "you will have",
+                "you're": "you are",
+                "you've": "you have",
+                "m'lord": "my lord",
+                "m'lady": "my lady",
+                "d'you": "do you"
+                } 
+        
+        self.TF_list=[]
+        self.tokenized_docs=[]
+    
+        for doc in docs:
+            # Handle contractions
+            #CODE
+            doc = doc.lower()
+            
+            for x in list(cList.keys()):
+                doc = re.sub(x, cList[x], doc)
+            
+            # Replace other uncommon contractions (mainly from GOT)
+            doc = re.sub("g'", "good ", doc)
+            doc = re.sub("m'", "my ", doc)
+            doc = re.sub("d'", "do ", doc)
+            doc = re.sub("'s", " is", doc)
+            
+            # Tokenize document
+            word_list = word_tokenize(doc)
+            
+            # Clean Text
+            word_list = clean_text(word_list)
+            
+            # Replace numbers with numerical representation
+            word_list = replace_numbers(word_list)
+            
+            self.tokenized_docs.append(word_list)
+            
+            BOW = get_BOW_TF(word_list)
+            
+            self.grams=[x for x in ngrams if x >=2 and x<=6]
+            
+            for size in self.grams:
+                tokenized_ngrams = ["_".join(word_list[x:x+size]) for x in range(len(word_list)) if len(word_list[x:x+size])== size]
+                NBOW = get_BOW_TF(tokenized_ngrams)
+                BOW.update(NBOW)
+            
+            self.TF_list.append(BOW)
+            
+            #self.TF_list=subsum_correction(TF_list)
+            
+        IDF_list = get_BOW_IDF(self.TF_list)
+        
+        self.tf_idf_list = get_TFIDF_info(tf_dicts=self.TF_list, idf_dict=IDF_list)
+        
+        self.cutoff=cutoff
+        self.take_top=take_top
+        
+    def stats(self):
+        PFO_factor={}
+        tl_factor={}
+        for doc in self.tokenized_docs:
+            PFO_factor.update(get_PFO(doc, self.cutoff))
+            tl_factor.update(get_TL(doc))
+            for size in self.grams:
+                tokenized_ngrams = ["_".join(doc[x:x+size]) for x in range(len(doc)) if len(doc[x:x+size])== size]
+                PFO_factor.update(get_PFO(tokenized_ngrams, self.cutoff))
+                tl_factor.update(get_TL(tokenized_ngrams))
+                
+        self.stat_ranking= self.tf_idf_list.copy()
+        
+        for i in range(len(self.stat_ranking)):
+            for key, val in self.stat_ranking[i].items():
+                self.stat_ranking[i][key]=val*PFO_factor[key]*tl_factor[key]
+            self.stat_ranking[i] = sort_dict(self.stat_ranking[i[:self.take_top]])
+            
+    def graph(self):
+#        node2word_dicts=[]
+        graph_list=[]
+        y=lambda x: x[0]
+        
+        for i in range(len(self.stat_ranking)):
+            
+#            doc_dict={}
+#        
+#            for j in range(self.take_top):
+#                x={self.stat_ranking[i][j][0]:j+1}
+#                doc_dict.update(x)
+#                
+#            node2word_dicts.append(doc_dict)
+            
+            ranking = self.stat_ranking[i]
+            words = [y(x) for x in ranking]
+            gram_lists = []
+            for i in [1].extend(self.grams):
+                gramz = [word for word in words if len(re.findall("_", word))==i-1]
+            G=nx.Graph()
+            G.add_nodes_from(words)
+            
+            #get edges of graphs
+            
+            graph_list.append(G)
+            
+            
+
+#if __name__ == "__main__":
+    
---
+++ Scraper.py
@@ -0,0 +1,127 @@
+# -*- coding: utf-8 -*-
+
+from bs4 import BeautifulSoup
+import requests
+import re
+import pandas as pd
+import json
+
+class Genius_TV_Scraper(object):
+    """This object scrapes Genius for scipts to game of thrones and the office"""
+    def __init__(self, show, seasons=None):
+        """Initialize Seasons and URLs with error handling"""
+        with open('data.json') as f:
+            config = json.load(f)
+
+        # base url should be placed in a config file
+        base_url = config["app"]["base_url"]
+
+        show = self.clean_show(show)
+
+        if show == "The office":
+            show = "The office us"
+
+        self.show = show
+        show_url = re.sub(" ", "-", show)
+        show_url = show_url + "/"
+
+        if show == "Game of thrones":
+            # GOT extension should be put in config file
+            extension = config["Game_of_Thrones"]["extension"]
+            GOT_season = config["Game_of_Thrones"]["seasons"]
+            if seasons is None:
+                seasons = range(1,GOT_season+1)
+            for i in seasons:
+                if i not in range(1, 8):
+                    raise ValueError("Episodes from Game of Thrones seasons 1-7 are available on Genius.")
+        elif show == "The office us":
+            # Office extension should be put in config file
+            extension = config["The_Office"]["extension"]
+            TO_Season = config["The_Office"]["seasons"]
+            if seasons is None:
+                seasons = range(1,TO_Season+1)
+            for i in seasons:
+                if i not in range(1,3):
+                    raise ValueError("Episodes from The Office US seasons 1-2 are available on Genius.")
+        else:
+            raise ValueError("Scraper pulls data from Game of Thrones or The Office. Set show equal to 'Game of thrones' or 'The office us'")
+        self.seasons = [i for i in seasons]
+        self.urls = [base_url+show_url+extension.format(i) for i in seasons]
+    
+    def clean_show(self, show):
+        """Clean the show name for processing"""
+        first_letter = show[0].upper()
+        rest_of_show = show[1:len(show)].lower()
+        show = first_letter + rest_of_show
+        return show
+
+    #fix issues with list comprehensions
+    def get_scripts(self, json=False):
+        """Get the scripts and return them as dataframes or JSON"""
+        show_df = pd.DataFrame()
+
+        for url in self.urls:
+            season_df = pd.DataFrame()
+            response = requests.get(url, timeout=5)
+            content = BeautifulSoup(response.content, "html.parser")
+            block = content.find_all("a", {"class": "u-display_block"}, href=True)
+            links = [b.get("href") for b in block]
+
+            for link in links:
+                # use regex  and re.sub to extract episode from URL (THE OFFICE)
+                pattern = re.compile("(?<=https://genius.com/Game-of-thrones-)(.*)(?=-annotated)")
+                match = re.search(pattern, link)
+                episode = match.group(0)
+                episode = re.sub("-", " ", episode)
+
+                #get soups for links
+                res = requests.get(link, timeout = 5)
+                episode_content = BeautifulSoup(res.content, "html.parser")
+
+                ep_script = episode_content.find_all("p")[0].text
+
+                # Create regex pattern to parse out names of speakers (first capture group) and the quotes (second capture group)
+                lines = re.findall(r"(?P<narration>(?<=^).*(?=\n\n)|(?<=\n\n).*(?=\n\n)|(?<=\n\n).*(?=$))", ep_script)
+                
+                names_n_quotes=[]
+                names = []
+                quotes =[]
+                line_counter = 0
+                for line in lines:
+                    lines2 = re.findall(r"(?P<name>(?<=^)[A-Z]{1}.*?(?=:| \()|(?<=\n)[A-Z]{1}.*?(?=:| \()).*: (?P<quote>.*)", line)
+                    names_n_quotes.append(lines2)
+                    if names_n_quotes[line_counter] == []:
+                        x = ""
+                        y = ""
+                    else:
+                        x= names_n_quotes[line_counter][0][0]
+                        y= names_n_quotes[line_counter][0][1]
+                    names.append(x)
+                    quotes.append(y)
+                    line_counter+=1
+
+                # create pandas dataframe to store name, quote, show, and episode
+                ep_dict = {"Character_Name": names,
+                            "Line": quotes,
+                            "Narration": lines,
+                            "Show": [self.show for s in range(0, len(lines))],
+                            "Episode": [episode.lower() for e in range(0, len(lines))],
+                            "URL": [link for l in range(0, len(lines))]}
+
+                ep_df = pd.DataFrame(data=ep_dict)
+
+                season_df = pd.concat([season_df, ep_df])
+
+            show_df = pd.concat([show_df, season_df])
+
+        if json == False:
+            return(show_df)
+
+        elif json == True:
+            json_data = show_df.to_json(orient="index")
+            json_data = re.sub(r"\\u2019", "’", json_data)
+            return(json_data)
+
+        else:
+            raise TypeError("JSON variable must be of type boolean.")
+
